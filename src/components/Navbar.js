45274969c59529caef9b774e893101a61c49350a
Fritz Johnson <fritzj96@gmail.com>
2019-02-22 21:28:53 -0800

    added react-router-dom & a link on the navbar-brand

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Navbar , Nav } from 'react-bootstrap';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const AppNavbar = () => {
     return (
         <Navbar bg="primary" variant="dark" expand="lg" fixed="top">
-            <Navbar.Brand>
-                Test
-            </Navbar.Brand>
+            <Link to="/">
+                <Navbar.Brand>
+                    Test
+                </Navbar.Brand>
+            </Link>
         </Navbar>
     )
 }
