8cf8182ae797c77920ceea9bce657830e8d39dec
Fritz Johnson <fritzj96@gmail.com>
2019-02-26 12:43:00 -0800

    added spacing to the top of the beer cards

--- src/components/Home/components/BeerCard.js
+++ src/components/Home/components/BeerCard.js
@@ -4,7 +4,7 @@ import { Col, Card } from 'react-bootstrap';
 const BeerCard = ({ name, id, tagline, imageUrl }) => {
   return (
     <Col md="4">
-      <Card bg="light">
+      <Card bg="light" className="mt-4 pt-4">
         <Card.Img variant="top" src={imageUrl} style={{ height: "10%", width: "10%", alignSelf: "center" }} />
         <Card.Body>
           <Card.Title className="text-center">{name}</Card.Title>
