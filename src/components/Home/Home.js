c2ab8e118c46b2b723707d8f65b6fae5eaa340cf
ottovbismarck <fritzj96@gmail.com>
2019-02-25 18:53:12 -0800

    setting up to add list of beer cards

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+
 import Searchbar from './components/Searchbar';
+import BeerCardList from './components/BeerCardList';
+
 const Home = () => {
     return (
         <Container>
@@ -9,6 +12,7 @@ const Home = () => {
                     <Searchbar />
                 </Col>
             </Row>
+            
         </Container>
     )
 }
