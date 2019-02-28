33da1d0a768ad229d19f124ebaf9bea194780243
ottovbismarck <fritzj96@gmail.com>
2019-02-27 19:24:09 -0800

    adding redux basics

--- src/data/store.js
+++ src/data/store.js
@@ -2,4 +2,6 @@ import { applyMiddleware, createStore } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-export const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from './data/store';
 import App from './App.js';
-ReactDOM.render(<App />, document.getElementById("root"));
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById("root"));
